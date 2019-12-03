285b36b3394484156b7be42f1f2347280e5e033f
yckai <thomasbataille96@gmail.com>
2019-12-03 23:58:10 +0100

    captcha

--- captcha.py
+++ captcha.py
@@ -3,7 +3,8 @@
 
 from binascii import a2b_base64
 from urllib.request import urlopen
-import tesserocr
+import tesseract
+import cv2.cv as cv
 url = "http://challenge01.root-me.org/programmation/ch8/index.php"
 html = urlopen(url)
 from bs4 import BeautifulSoup
@@ -15,5 +16,13 @@ for res in soup.findAll('img'):
   fd = open('image.png', 'wb')
   fd.write(binary_data)
   fd.close()
+  grayScale = cv.LoadImage('image.jpeg', cv.CV_LOAD_IMAGE_GRAYSCALE)
+  cv.Threshold(gray, gray, 231, 255, cv.CV_THRESH_BINARY)
+  api = tesseract.TessBaseAPI()
+  api.Init(".", "eng", tesseract.OEM_DEFAULT)
+  api.SetVariable("tessedit_char_whitelist", "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  api.SetPageSegMode(tesseract.PSM_SINGLE_WORD)
+  tesseract.SetCvImage(gray, api)
+  print (api.GetUTF8Text())
 
 
